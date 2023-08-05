b28ae19132c12c78e7d515481934e0e0dc55dc61
James Carter <xpb20111@archie-t.hpc.strath.ac.uk>
2023-08-05 02:58:20 +0100

    Add ext_test_sets to get_average_model_performance()

--- training_utils/model_scoring.py
+++ training_utils/model_scoring.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-def get_average_model_performance(df_final_results, mode='regression'): 
+def get_average_model_performance(df_final_results, mode='regression', ext_test_sets=[]):
 
     if mode == 'regression':
         # Get average, standard deviation and standard error:
         df_av_results = pd.DataFrame()
-        for dataset in ['train', 'test']:
+
+        for dataset in ['train', 'test'] + ext_test_sets:
             df_agg = df_final_results[dataset].agg(['mean', 'std', 'sem']).T
             df_agg.index.rename('metric', inplace=True)
-        
+
             # Calculate confidence intervals:
             df_agg.loc['rmsd', 'CI95_lower'] = df_agg.loc['rmsd', 'mean']-1.69*df_agg.loc['rmsd', 'sem']
             df_agg.loc['rmsd', 'CI95_upper'] = df_agg.loc['rmsd', 'mean']+1.69*df_agg.loc['rmsd', 'sem']
