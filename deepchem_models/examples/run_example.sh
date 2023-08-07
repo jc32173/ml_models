36baa09d3d003f7197ecae3269acb8e17febcac1
James Carter <xpb20111@archie-t.hpc.strath.ac.uk>
2023-08-07 02:05:15 +0100

    Update run_params.json for the example and delete files from previous example runs

--- deepchem_models/examples/run_example.sh
+++ deepchem_models/examples/run_example.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+# Remove any files from previous run:
+
+rm -fr train_val_test_split.csv predictions.csv \
+       GCNN_info_all_models.csv GCNN_info_refit_models.csv \
+       GraphConvModel_info_av_performance.csv \
+       resample_0_cv_refit_model_1 resample_1_cv_refit_model_0
+
 # Run example using parameters in run_params.json file and
 # with random seed set to 0 to make results reproducible:
 
